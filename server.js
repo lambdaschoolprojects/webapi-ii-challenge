074eb92b69b7d25410c14deaf54e6dafdb125d05
Rane <github@blasph.me>
2019-07-31 18:52:23 -0700

    clean up after merge

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const PostRouter = require('./posts/post-routes');
+const express = require("express");
+const PostRouter = require("./posts/post-routes");
 const server = express();
 
-
 server.use(express.json());
-server.use('/api/posts', PostRouter);
+server.use("/api/posts", PostRouter);
 
-server.get('/', (req, res) => {
-    res.send(`
+server.get("/", (req, res) => {
+  res.send(`
     <h2>Welcome to the Server</h2>
     <h3>We got fun and games</h3>
     
     `);
-})
+});
 
 module.exports = server;
